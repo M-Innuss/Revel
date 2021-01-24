639d6f37696126e5dd395cd005689a477ce72a4c
Martins Innuss <57411010+M-Innuss@users.noreply.github.com>
2021-01-24 12:09:05 +0100

    Update Account.go

--- User/Account.go
+++ User/Account.go
@@ -51,7 +51,7 @@ func InsertAccount(db *sql.DB, account Account) (sql.Result, error) {
 func SelectAccount(db *sql.DB, IdNumber uint, Email string, DeviceId uint, result *Account) error {
 	row := db.QueryRow(
 		fmt.Sprintf(
-			"SELECT * FROM %s WHERE %s=$1 AND %s=$2 AND %s=$3",
+			"SELECT * FROM %s WHERE %s AND %s AND %s",
 			AccountTableName,
 			AccountIdNumberCol,
 			AccountEmailCol,
